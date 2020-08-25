a1880111496326e704a61ed82da750e74a218f18
Andrew Vuong <andrew.vuong@berkeley.edu>
2020-08-24 17:01:27 -0700

    Refactored the dancer/blinkydancer classes to pseudoclassical

--- src/dancer.js
+++ src/dancer.js
@@ -3,17 +3,18 @@ var makeDancer = function(top, left, timeBetweenSteps) {
 
   // use jQuery to create an HTML <span> tag
   this.$node = $('<span class="dancer"></span>'); // this.node
-
+  this.step();
+  // this one sets the position to some random default point within the body
+  this.setPosition(top, left);
+  this.timeBetweenSteps = timeBetweenSteps;
 };
 
 // change to prototype method
 makeDancer.prototype.step = function() {
   // the basic dancer doesn't do anything interesting at all on each step,
   // it just schedules the next step
-  setTimeout(dancer.step, timeBetweenSteps);
+  setTimeout(this.step.bind(this), this.timeBetweenSteps);
 };
-this.step();
-
 // change to prototype method
 makeDancer.prototype.setPosition = function(top, left) {
   // Use css top and left properties to position our <span> tag
@@ -23,9 +24,7 @@ makeDancer.prototype.setPosition = function(top, left) {
     top: top,
     left: left
   };
-  dancer.$node.css(styleSettings);
+  this.$node.css(styleSettings);
 };
 
-// now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
-// this one sets the position to some random default point within the body
-this.setPosition(top, left);
+
